e664698f362d1b516a4eca986ae8e7756cab4e64
calliope <calliope183@gmail.com>
2023-02-28 23:56:03 +0900

    add

--- homework/libs/slide_min.py
+++ homework/libs/slide_min.py
@@ -1,30 +1,39 @@
 from collections import deque
-from typing import NewType
+from typing import cast, TypeAlias, TypeVar
 
-Index = NewType("Index", int)
-Value = NewType("Value", int)
+Index: TypeAlias = int
+Value = TypeVar('Value', int , str)
 
 
 class SlideMin:
     deq: deque[tuple[Index, Value]]
     section_length: int
 
-    def __init__(self, init_value: int, section_length: int) -> None:
-        self.deq = deque([(Index(0), Value(init_value))])
+    def __init__(self, init_value: Value, section_length: int) -> None:
+        self.deq = deque([(0, init_value)])
         self.section_length = section_length
 
-    def add(self, new_value: int) -> tuple[Index, Value]:
-        new_index = self.deq[-1][0] + 1
+    def add(self, new_value: Value) -> tuple[Index, Value]:
+        new_index: Index = cast(Index, self.deq[-1][0] + 1)
         while len(self.deq) and self.deq[-1][1] >= new_value:
             self.deq.pop()
         if len(self.deq) and self.deq[0][0] <= new_index - self.section_length:
             self.deq.popleft()
-        self.deq.append((Index(new_index), Value(new_value)))
+        self.deq.append((new_index, new_value))
+        return self.min
+
+    @property
+    def last_value(self):
+        return self.deq[-1][1]
+
+    @property
+    def min(self):
         return self.deq[0]
 
 
 if __name__ == "__main__":
-    a = ['AC', 'AA', 'CG', 'CT', 'GC', 'AA']
-    s = SlideMin(33, 1)
+    a = ["AC", "AA", "CG", "CT", "GC", "AA"]
+    s = SlideMin("TT", 2)
     for v in a:
+        print(s.deq)
         print(s.add(v))
--- homework/one.py
+++ homework/one.py
@@ -1,14 +1,16 @@
 import collections
+from functools import cache
 from collections.abc import Iterator
 from datetime import datetime
 from pathlib import Path
-from typing import ClassVar, Literal, NewType, Type
+from typing import Literal, TypeAlias
+from time import perf_counter
 
 from libs.slide_min import Index, SlideMin, Value
 
-Minimizer = NewType("Minimizer", int)
-RefId = NewType("RefId", str)
-ReadId = NewType("ReadId", str)
+Minimizer: TypeAlias = int
+RefId: TypeAlias = str
+ReadId: TypeAlias = str
 Strand = Literal[0, 1]
 
 CURRENT_FOLDER_DIR = Path(__file__).resolve().parent
@@ -19,9 +21,6 @@ NUCLEOTIDE_TO_INTEGER_MAPPING: dict[
 
 
 class Minimap:
-    cached_seq_dict: ClassVar[
-        dict[tuple[Minimizer, Literal["A", "C", "G", "T"]], tuple[Minimizer, Minimizer]]
-    ]
     read_seq_file_path: Path
     ref_seq_file_path: Path
     mapping_result_output_file_path: Path
@@ -45,11 +44,8 @@ class Minimap:
             if mapping_result_output_file_path is None
             else mapping_result_output_file_path
         )
-        self.cached_seq_dict = (
-            {} if self.cached_seq_dict is None else self.cached_seq_dict
-        )
         self.k_mer_size = k_mer_size
-        self.k_mer_hash_mod = 1 << ((k_mer_size - 1) * 2)
+        self.k_mer_hash_mod = 1 << ((k_mer_size - 1) * 2)  # = 4 ** (k_mer_size - 1)
         self.window_size = window_size
 
     def parse_ref_seq_file(self) -> dict[RefId, str]:
@@ -66,7 +62,7 @@ class Minimap:
                     if seq != "":
                         ref_seq_dict[tmp_ref_id] = seq
                         seq = ""
-                    tmp_ref_id = RefId(line.split()[0][1:])
+                    tmp_ref_id = line.split()[0][1:]
                 else:
                     seq += line
             ref_seq_dict[tmp_ref_id] = seq
@@ -76,7 +72,7 @@ class Minimap:
         read_seq_dict: dict[ReadId, str] = {}
         with self.read_seq_file_path.open("r") as f:
             while True:
-                read_id = ReadId(f.readline().rstrip()[1:])
+                read_id = f.readline().rstrip()[1:]
                 read_array = f.readline().rstrip()
                 f.readline()
                 f.readline()
@@ -88,7 +84,7 @@ class Minimap:
     def sketch_minimizers(self, seq: str) -> Iterator[tuple[Value, Index, Strand]]:
         window_size = self.window_size
         k_mer_size = self.k_mer_size
-        minimizers_set: set[tuple[Minimizer, Index, Strand]] = set()
+        minimizers_set: set[tuple[Index, Strand]] = set()
         seq_length = len(seq)
         k_mer_init = seq[:k_mer_size]
         k_mer_hashes_strand_0 = SlideMin(self.seq2hash(k_mer_init), window_size)
@@ -96,8 +92,8 @@ class Minimap:
             self.seq2hash(k_mer_init, strand=1), window_size
         )
         for i in range(window_size - 1):
-            last_k_mer_hash_strand_0_value = k_mer_hashes_strand_0.deq[-1][1]
-            last_k_mer_hash_strand_1_value = k_mer_hashes_strand_1.deq[-1][1]
+            last_k_mer_hash_strand_0_value = k_mer_hashes_strand_0.last_value
+            last_k_mer_hash_strand_1_value = k_mer_hashes_strand_1.last_value
             (
                 new_k_mer_hash_strand_0_value,
                 new_k_mer_hash_strand_1_value,
@@ -112,25 +108,23 @@ class Minimap:
             (
                 k_mer_hash_strand_0_index,
                 k_mer_hash_strand_0_value,
-            ) = k_mer_hashes_strand_0.deq[0]
+            ) = k_mer_hashes_strand_0.min
             (
                 k_mer_hash_strand_1_index,
                 k_mer_hash_strand_1_value,
-            ) = k_mer_hashes_strand_1.deq[0]
-            if k_mer_hash_strand_0_value < k_mer_hash_strand_1_value:
-                minimizers_set.add(
-                    (k_mer_hash_strand_0_value, k_mer_hash_strand_0_index, 0)
-                )
+            ) = k_mer_hashes_strand_1.min
+            if k_mer_hash_strand_0_value <= k_mer_hash_strand_1_value:
+                minimizers_set.add((k_mer_hash_strand_0_index, 0))
+                # yield k_mer_hash_strand_0_value, 0, 0
                 yield k_mer_hash_strand_0_value, k_mer_hash_strand_0_index, 0
-            if k_mer_hash_strand_0_value > k_mer_hash_strand_1_value:
-                minimizers_set.add(
-                    (k_mer_hash_strand_1_value, k_mer_hash_strand_1_index, 1)
-                )
+            else:
+                minimizers_set.add((k_mer_hash_strand_1_index, 1))
+                # yield k_mer_hash_strand_1_value, 0, 1
                 yield k_mer_hash_strand_1_value, k_mer_hash_strand_1_index, 1
 
         for j in range(seq_length - k_mer_size - window_size + 1):
-            last_k_mer_hash_strand_0_value = k_mer_hashes_strand_0.deq[-1][1]
-            last_k_mer_hash_strand_1_value = k_mer_hashes_strand_1.deq[-1][1]
+            last_k_mer_hash_strand_0_value = k_mer_hashes_strand_0.last_value
+            last_k_mer_hash_strand_1_value = k_mer_hashes_strand_1.last_value
             (
                 new_k_mer_hash_strand_0_value,
                 new_k_mer_hash_strand_1_value,
@@ -149,25 +143,21 @@ class Minimap:
                 k_mer_hash_strand_1_value,
             ) = k_mer_hashes_strand_1.add(new_k_mer_hash_strand_1_value)
             if (
-                k_mer_hash_strand_0_value < k_mer_hash_strand_1_value
-                and (k_mer_hash_strand_0_value, k_mer_hash_strand_0_index, 0)
-                not in minimizers_set
+                k_mer_hash_strand_0_value <= k_mer_hash_strand_1_value
+                and (k_mer_hash_strand_0_index, 0) not in minimizers_set
             ):
-                minimizers_set.add(
-                    (k_mer_hash_strand_0_value, k_mer_hash_strand_0_index, 0)
-                )
+                minimizers_set.add((k_mer_hash_strand_0_index, 0))
+                # yield k_mer_hash_strand_0_value, j + 1, 0
                 yield k_mer_hash_strand_0_value, k_mer_hash_strand_0_index, 0
             if (
                 k_mer_hash_strand_0_value > k_mer_hash_strand_1_value
-                and (k_mer_hash_strand_1_value, k_mer_hash_strand_1_index, 1)
-                not in minimizers_set
+                and (k_mer_hash_strand_1_index, 1) not in minimizers_set
             ):
-                minimizers_set.add(
-                    (k_mer_hash_strand_1_value, k_mer_hash_strand_1_index, 1)
-                )
-                yield k_mer_hash_strand_1_value, k_mer_hash_strand_1_index, 1
+                minimizers_set.add((k_mer_hash_strand_1_index, 1))
+                # yield k_mer_hash_strand_1_value, j + 1, 1
+                yield k_mer_hash_strand_1_value, k_mer_hash_strand_0_index, 1
 
-    def run(self, hits_length_threshold=5) -> None:
+    def run(self) -> None:
         ref_seq_dict: dict[RefId, str] = self.parse_ref_seq_file()
         read_seq_dict: dict[ReadId, str] = self.parse_read_seq_file()
 
@@ -175,30 +165,59 @@ class Minimap:
             Minimizer, set[tuple[RefId, Index, Strand]]
         ] = collections.defaultdict(set)
         for ref_id, ref_seq in ref_seq_dict.items():
-            minimizers_list = self.sketch_minimizers(ref_seq)
-            for (minimizer, ref_pos, strand) in minimizers_list:
+            for (minimizer, ref_pos, strand) in self.sketch_minimizers(ref_seq):
                 ref_minimizer_dict[minimizer].add((ref_id, ref_pos, strand))
+        # print(ref_minimizer_dict)
 
         outputs = []
         for read_id, read_seq in read_seq_dict.items():
             # hits: dict[tuple[RefId, Strand], Index] = {}
+            # hits: collections.defaultdict[
+            #     tuple[RefId, Strand], list[tuple[Index, Index]]
+            # ] = collections.defaultdict(list)
+            read_seq_length = len(read_seq)
+            overlap_index_difference = (
+                read_seq_length - self.window_size - self.k_mer_size + 1
+            )
             hits: collections.defaultdict[
-                tuple[RefId, Strand], list[tuple[Index, Index]]
-            ] = collections.defaultdict(list)
-            minimizers_list = self.sketch_minimizers(read_seq)
-            for (read_minimizer, read_pos, read_strand) in minimizers_list:
-                print(read_minimizer, read_pos, read_strand)
+                RefId,
+                collections.Counter,
+            ] = collections.defaultdict(collections.Counter)
+            for (read_minimizer, read_pos, read_strand) in self.sketch_minimizers(read_seq):
                 for (ref_id, ref_pos, ref_strand) in ref_minimizer_dict[read_minimizer]:
+                    # print(read_minimizer, read_pos, read_strand, ref_id, ref_pos, ref_strand)
                     if read_strand == ref_strand:
+                        hits[ref_id][ref_pos - read_pos] += 1
                         # hits[(ref_id, 0)] = min(ref_pos-read_pos, hits.get((ref_id, 0), ref_pos-read_pos))
-                        hits[(ref_id, 0)].append((ref_pos, read_pos))
+                        # hits[(ref_id, 0)].append((ref_pos, read_pos))
                     else:
-                        hits[(ref_id, 1)].append((ref_pos, read_pos))
-                        # hits[(ref_id, 1)] = min(ref_pos*2-read_pos, hits.get((ref_id, 1), ref_pos-read_pos))
+                        hits[ref_id][
+                            -(ref_pos + read_pos - overlap_index_difference)
+                        ] += 1
+                        # hits[(ref_id, 1)] = min(ref_pos-read_pos, hits.get((ref_id, 1), ref_pos-read_pos))
+                        # hits[(ref_id, 1)].append((ref_pos, read_pos))
+            else:
+                score = 0
+                output = ""
+                for ref_id, counter in hits.items():
+                    most_common_hit = counter.most_common(1)[0]
+                    if most_common_hit[1] > score:
+                        score = most_common_hit[1]
+                        pos = (
+                            most_common_hit[0] + 1
+                            if most_common_hit[0] >= 0
+                            else -most_common_hit[0] + 1
+                        )
+                        output = self.format(
+                            read_id, ref_id, pos, int(most_common_hit[0] < 0)
+                        )
+                else:
+                    outputs.append(output)
+                # hits[(ref_id, 1)] = min(ref_pos*2-read_pos, hits.get((ref_id, 1), ref_pos-read_pos))
 
-            print(read_id)
-            print(hits)
-            print()
+            # print(read_id)
+            # print(hits)
+            # print()
             # for (ref_id, strand_order), ref_pos in hits.items():
             #     if strand_order == 0 and read_seq[0] == ref_seq_dict[ref_id][ref_pos]:
             #         outputs.append(self.format(read_id, ref_id, ref_pos+1, 0))
@@ -212,6 +231,7 @@ class Minimap:
     def format(self, read_id: ReadId, ref_id: RefId, pos: int, strand_order: Strand):
         return f'{read_id}\t{ref_id}\t{pos}\t{"+-"[strand_order]}\n'
 
+    @cache
     def calc_hash(
         self,
         last_k_mer_hash_strand_0_value: Minimizer,
@@ -219,67 +239,42 @@ class Minimap:
         next_nucleotide: Literal["A", "C", "G", "T"],
     ) -> tuple[Minimizer, Minimizer]:
         k_mer_hash_mod = self.k_mer_hash_mod
-        if (last_k_mer_hash_strand_0_value, next_nucleotide) in self.cached_seq_dict:
-            return self.cached_seq_dict[
-                (last_k_mer_hash_strand_0_value, next_nucleotide)
-            ]
-
         k_mer_hash_strand_0_value = (
             last_k_mer_hash_strand_0_value % k_mer_hash_mod
         ) * 4 + self.seq2hash(next_nucleotide)
         k_mer_hash_strand_1_value = (
             last_k_mer_hash_strand_1_value >> 2
         ) + self.seq2hash(next_nucleotide, strand=1) * k_mer_hash_mod
-        self.update_cached_seq_dict(
-            (last_k_mer_hash_strand_0_value, next_nucleotide),
-            (k_mer_hash_strand_0_value, k_mer_hash_strand_1_value),
-        )
         return k_mer_hash_strand_0_value, k_mer_hash_strand_1_value
 
-    def update_cached_seq_dict(
-        self,
-        key: tuple[Minimizer, Literal["A", "C", "G", "T"]],
-        value: tuple[Minimizer, Minimizer],
-    ) -> None:
-        self.cached_seq_dict[key] = value
-
     @staticmethod
+    @cache
     def seq2hash(seq: str, *, strand: Strand = 0) -> int:
         res: int = 0
         if strand == 0:
             for i, v in enumerate(seq[::-1]):
                 res += NUCLEOTIDE_TO_INTEGER_MAPPING[v] * 4**i
         else:
-            for i, v in enumerate(seq):
+            for i, v in enumerate(seq[::-1]):
                 res += (3 - NUCLEOTIDE_TO_INTEGER_MAPPING[v]) * 4**i
         return res
 
-    @classmethod
-    def minimap_cls_factory(
-        cls: Type["Minimap"],
-        cached_seq_dict: dict[
-            tuple[Minimizer, Literal["A", "C", "G", "T"]], tuple[Minimizer, Minimizer]
-        ],
-    ) -> Type["Minimap"]:
-        cls.cached_seq_dict = cached_seq_dict
-        return cls
-
 
 if __name__ == "__main__":
-    IS_TEST = True
+    IS_TEST = False
     if IS_TEST:
-        minimap_factory = Minimap.minimap_cls_factory({})
-        minimap = minimap_factory(
+        minimap = Minimap(
             CURRENT_FOLDER_DIR / "test" / "read.fastq",
             CURRENT_FOLDER_DIR / "test" / "ref.fasta",
-            window_size=1,
-            k_mer_size=3,
+            window_size=10,
+            k_mer_size=20,
         )
         minimap.run()
     else:
-        minimap_factory = Minimap.minimap_cls_factory({})
-        minimap = minimap_factory(
-            CURRENT_FOLDER_DIR / "test" / "read.fastq",
-            CURRENT_FOLDER_DIR / "test" / "ref.fasta",
+        minimap = Minimap(
+            CURRENT_FOLDER_DIR.parent / "SE11" / "Illumina_SE11.fastq",
+            CURRENT_FOLDER_DIR.parent / "SE11" / "ref_SE11.fasta",
+            window_size=10,
+            k_mer_size=20,
         )
-        minimap.run(window_size=4, k_mer_size=25)
+        minimap.run()
